5edee3e3d4e42467bcd909778fc24aaa71366ac5
dimahvirstuy <dhvirtsman@stuy.edu>
2017-10-16 11:46:15 -0400

    a

--- list.c
+++ list.c
@@ -1,24 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct node {
-  int i;
-  struct node * next;
+typedef struct node{ 
+  char name[256];
+  char artist[256];
+  struct node *next;
 };
 
-struct node * insert_front(struct node * old_front, int val) {
-  
+struct node * insert_front(struct node * old_front, char* art, char * nam) {
   struct node * new_front;
   //printf("made new_front\n");
   new_front=(struct node *)malloc(sizeof(struct node));
   //printf("malloc successful\n");
-  new_front->i= val;
+  new_front->artist= art;
+  new_front->name= nam;
   new_front->next=old_front;
   //printf("insert_front successful\n");
   return new_front;
 
 }
 
+struct node * insert_order(struct node * old_front, char* art, char * nam) {
+  struct node * new_node;
+  //printf("made new_front\n");
+  new_node=(struct node *)malloc(sizeof(struct node));
+  //printf("malloc successful\n");
+  new_node->artist= art;
+  new_node->name= nam;
+  while (1) {
+    if (! strcmp(art, old_front->artist)) {
+      if (strcmp(nam, old_front->name) > 0) {
+	new_node->next = old_front->next;
+	old_front->next = new_node;
+      }
+      return new_node;
+    }
+    if (strcmp(art, old_front->artist) > 0) {
+      //do the same as ^^^^
+    }
+  }
+  //new_node->next=old_front;
+  //printf("insert_front successful\n");
+  return new_front;
+
+}
 void print_list(struct node * list) {
 
   printf("FRONT-->");
